dca55e4c8e4e0576d19a55f409a669cf3c13db80
Cheton Wu <cheton@gmail.com>
2016-06-13 13:27:57 +0800

    Defaults to div if tag is not specified

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import escapeHTML from 'escape-html';
 
-module.exports = (tag = 'div', attrs = {}, text) => {
+// @param {string} [tag] The tag name. Defaults to 'div'.
+// @param {object} attrs HTML attributes.
+// @param {string} [text] The content string.
+module.exports = (tag, attrs, text) => {
+    if (typeof tag === 'object') {
+        text = attrs;
+        attrs = tag;
+        tag = 'div';
+    }
+
     let html = '<' + tag;
 
+    attrs = { ...attrs };
     Object.keys(attrs).forEach(name => {
         if (name) {
             let value = escapeHTML(attrs[name]);
             html += ' ' + name + '="' + value + '"';
         }
     });
+
     html += (text !== undefined) ? '>' + text + '</' + tag + '>' : '/>';
 
     return html;
--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
 import { test } from 'tap';
 import tag from '../src';
 
-test('a tag', (t) => {
+test('Defaults to div if tag is not specified', (t) => {
+    const result = tag({
+        class: 'container'
+    }, 'container text');
+    const wanted = '<div class="container">container text</div>';
+    t.same(result, wanted);
+    t.end();
+});
+
+test('A tag', (t) => {
     const result = tag('a', {
         class: 'btn btn-link',
         href: 'http://github.com/cheton/html-tag',
@@ -11,7 +20,7 @@ test('a tag', (t) => {
     t.end();
 });
 
-test('input tag', (t) => {
+test('INPUT tag', (t) => {
     const result = tag('input', {
         name: 'name',
         value: 'Input your name...'
